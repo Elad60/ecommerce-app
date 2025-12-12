add02fe530427bfde5b15e27791258eb0020865e
EladaKubiya <elada@kubiyax.com>
2025-12-12 19:48:51 +0200

    Routing and basic structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Products } from './pages/products/products';
+import { Cart } from './pages/cart/cart';
+import { Auth } from './pages/auth/auth';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: 'home', component: Home},
+    { path: 'products', component: Products},
+    { path: 'cart', component: Cart},
+    { path: 'auth', component: Auth},
+];
